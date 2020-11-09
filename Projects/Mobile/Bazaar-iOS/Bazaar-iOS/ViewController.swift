f7fd0e4e291fb9aacd71ebd8637cf6abfa430607
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-09 12:57:24 +0300

    Add UITableView to display product

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/ViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var productTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
