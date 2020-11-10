d9a50649c83db5b7b331ef70efa5a6bbc1eec5e4
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-10 17:12:21 +0300

    added create_user and delete_user
    should add try catch later on
    need unit tests

--- Projects/Backend/src/product/admin.py
+++ Projects/Backend/src/product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from product.models import Product,Order,Cart,Comment,ProductList
 # Register your models here.
+
+admin.site.register(Product)
+
+admin.site.register(Order)
+
+admin.site.register(Cart)
+
+admin.site.register(Comment)
+
+admin.site.register(ProductList)
