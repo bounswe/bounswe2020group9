0805f1fcfdc6bc4da85537039d33c13357a4f334
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-11-10 23:31:03 +0300

    Added new pages

--- Projects/Frontend/bazaar-frontend/src/screens/Sign-In/index.js
+++ Projects/Frontend/bazaar-frontend/src/screens/Sign-In/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const SignIn = () => {
-    return (
-        <div>
-            HERE SIGN IN PAGE
-        </div>
-    );
+  return (
+    <div>
+      SIGN IN PAGE
+    </div>
+  );
 }
 
 export default SignIn;
