c51933452564b4432ea3a9aa638aac8f66a2c2de
alcanunsal <alcanunsal@gmail.com>
2020-11-13 17:57:46 +0300

    some minor improvements in table cell design

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/MyCartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyCartViewController.swift
+//  Bazaar-iOS
+//
+//  Created by alc on 12.11.2020.
+//
+
+import UIKit
+
+class MyCartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
