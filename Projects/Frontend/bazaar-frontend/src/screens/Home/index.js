d24670813f68d1105f770a824cf99f1b18b4e71e
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-11-15 00:16:20 +0300

    FIrst Changes

--- Projects/Frontend/bazaar-frontend/src/screens/Home/index.js
+++ Projects/Frontend/bazaar-frontend/src/screens/Home/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
 
 const Home = () => {
   return (
@@ -18,6 +18,13 @@ const Home = () => {
             </li>
           </ul>
         </nav>
+        <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+          Library
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Data</Breadcrumb.Item>
+      </Breadcrumb>
     </div>
   );
 }
