b2843da579133e9587e7535c132fde1a47f17f7d
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-18 19:25:43 +0300

    Add SignUpLoginViewController

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/SignUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 18.11.2020.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
