6545b22ad9a7d1bffac879ac7c4565bde1856384
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-18 19:25:26 +0300

    Add LoginViewController

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 18.11.2020.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
