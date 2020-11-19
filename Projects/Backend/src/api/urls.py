51a8376a8e40d2fce238b69eb35f426b7fdae4b5
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-19 17:27:11 +0300

    changed debug=False while supporting static css

--- Projects/Backend/src/api/urls.py
+++ Projects/Backend/src/api/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include("product.urls")),
     path('admin/', admin.site.urls),
     path('api/product/', include("product.urls")),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
