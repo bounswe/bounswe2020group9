a04980259697b2e3dd0901d3af91eeb989c90b38
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-20 19:26:38 +0300

    Update views.py

--- Projects/Backend/src/product/views.py
+++ Projects/Backend/src/product/views.py
@@ -1,14 +1,58 @@
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
+
 from product.models import Product
 from product.serializers import ProductSerializer
+
+
 # Create your views here.
 
 
-def get_product(request):
-    return HttpResponse("Hello World!")
+class ProductListAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
+class ProductDetailAPIView(APIView):
+
+    def get_product(self, id):
+        try:
+            Product.objects.get(id=id)
+        except Product.DoesNotExist:
+            return HttpResponse(status=HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        product = self.get_product(id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        product = self.get_product(id)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        product = self.get_product(id)
+        product.delete()
+        return HttpResponse(status=HTTP_204_NO_CONTENT)
+
+
+"""
 def product_list(request):
     if request.method == "GET":
         products = Product.objects.all()
@@ -45,3 +89,5 @@ def product_detail(request, pk):
     elif request.method == "DELETE":
         product.delete()
         return HttpResponse(status=204)
+        
+"""
