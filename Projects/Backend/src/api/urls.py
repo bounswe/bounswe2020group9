24f3899242830b81bf5feb0a65d9a2e12963bbb0
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-20 19:19:36 +0300

    modified product for better implementation

--- Projects/Backend/src/api/urls.py
+++ Projects/Backend/src/api/urls.py
@@ -1,4 +1,4 @@
-The `urlpatterns` list routes URLs to views. For more information please see:
+"""The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
--- Projects/Backend/src/product/views.py
+++ Projects/Backend/src/product/views.py
@@ -1,28 +1,72 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
 from product.models import Product
 from product.serializers import ProductSerializer
 # Create your views here.
 
 
-def get_product(request):
-    return HttpResponse("Hello World!")
+class ProductListAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return  Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+class ProductDetailAPIView(APIView):
+
+    def get_product(self, id):
+        try:
+            Product.objects.get(id=id)
+        except Product.DoesNotExist:
+            return HttpResponse(status=404)
+
+    def get(self, request, id):
+        product = self.get_product(id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        product = self.get_product(id)
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(product,data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, id):
+        product = self.get_product(id)
+        product.delete()
+        return HttpResponse(status=204)
+
+"""
 
 @api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == "GET":
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data, safe=False)
 
     elif request.method == "POST":
         data = JSONParser().parse(request)
         serializer = ProductSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return  JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=201)
+        return  Response(serializer.errors, status=400)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -34,16 +78,18 @@ def product_detail(request, pk):
 
     if request.method == "GET":
         serializer = ProductSerializer(product)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
         data = JSONParser().parse(request)
         serializer = ProductSerializer(product,data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
 
     elif request.method == "DELETE":
         product.delete()
         return HttpResponse(status=204)
+        
+"""
