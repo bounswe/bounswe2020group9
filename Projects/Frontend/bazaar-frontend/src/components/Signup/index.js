b74ce132c0e01201375d37dd1a8ac0afbc80b716
firatbulut19 <44057526+firatbulut19@users.noreply.github.com>
2020-11-21 22:49:14 +0300

    profile page implemented, minor changes to signin and signup pages

--- Projects/Frontend/bazaar-frontend/src/components/Signup/index.js
+++ Projects/Frontend/bazaar-frontend/src/components/Signup/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import GoogleButton from 'react-google-button'
 
+import "./signup.css";
+
 export default class SignUpComponent extends Component {
     render() {
         return (
             <div className="entry-form">
                 <form >
                     <h3>Sign Up</h3>
-
+                    <div className="row">
+                        <div className="form-group col">
+                            <label>First Name</label>
+                            <input type="name" className="form-control" placeholder="Enter name" />
+                        </div>
+                        <div className="form-group col">
+                            <label>Surname</label>
+                            <input type="surname" className="form-control" placeholder="Enter surname" />
+                        </div>
+                    </div>
                     <div className="form-group">
                         <label>Email address</label>
                         <input type="email" className="form-control" placeholder="Enter email" />
