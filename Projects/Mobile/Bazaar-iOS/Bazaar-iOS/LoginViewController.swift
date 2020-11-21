328eb7d49578a8b6a8873e4cb5fb8dd4065e87ac
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-21 16:16:00 +0300

    Add sign up page design

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/LoginViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/LoginViewController.swift
@@ -12,11 +12,12 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var frameView: UIView!
+    let alertController = UIAlertController(title: "Alert!", message: "Message", preferredStyle: .alert)
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         frameView.layer.borderColor = #colorLiteral(red: 1, green: 0.6431372549, blue: 0.3568627451, alpha: 1)
         frameView.layer.shadowColor = UIColor.black.cgColor
-        
     }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
@@ -24,27 +25,21 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Alert!", message: "Message", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
         
-        
-        var loginSuccessful = false
         if let email = emailTextField.text {
             if !email.isEmail {
                 alertController.message = "Your email address is invalid. Please enter a valid address."
                 self.present(alertController, animated: true, completion: nil)
-                loginSuccessful = false
             }else{
                 if let password = passwordTextField.text{
                     if password.count < 8 || password.count > 20 {
                         alertController.message = "Password must be at least 8 , at most 20 characters in length"
                         self.present(alertController, animated: true, completion: nil)
-                        loginSuccessful = false
                     }else {
-                        loginSuccessful = true
+                        performSegue(withIdentifier: "loginToMain", sender: nil)
                     }
                 }else{
-                    loginSuccessful = false
                 }
             }
         }else{
