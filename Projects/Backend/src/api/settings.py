810fd96222df212782dd702102fe03fa40f8d1af
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-22 22:24:01 +0300

    many to many bugfix

--- Projects/Backend/src/api/settings.py
+++ Projects/Backend/src/api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x50$_&l5@ui^4=9)p5^xz&1bnyctdj+9dipourzc6s$d2bu3fr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
