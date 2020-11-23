cfab0a017be9d88b324c5a78fffbcc9ebd0d899a
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-23 18:16:04 +0300

    Add URLRequestBuilder file

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Model/URLRequestBuilder.swift
@@ -0,0 +1,62 @@
+//
+//  URLRequestBuilder.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 23.11.2020.
+//
+
+import Foundation
+
+protocol URLRequestBuilder: URLRequestConvertible {
+    
+    var mainURL: URL { get }
+    var requestURL: URL { get }
+    // MARK: - Path
+    var path: String { get }
+    
+    // MARK: - Parameters
+    var parameters: Parameters? { get }
+    var headers: HTTPHeaders? { get}
+    
+    // MARK: - Methods
+    var method: HTTPMethod { get }
+    var encoding: ParameterEncoding { get }
+    var urlRequest: URLRequest { get }
+}
+
+
+extension URLRequestBuilder {
+    var encoding: ParameterEncoding {
+        switch method {
+        case .get:
+            return URLEncoding.default
+        default:
+            return JSONEncoding.default
+        }
+    }
+    
+    var mainURL: URL  {
+        return URL(string: "")!
+    }
+    
+    var requestURL: URL {
+        return mainURL.appendingPathComponent(path)
+    }
+    
+    var urlRequest: URLRequest {
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = method.rawValue
+        return request
+    }
+    
+    // MARK: - URLRequestConvertible
+    func asURLRequest() throws -> URLRequest {
+        var result = try encoding.encode(urlRequest, with: parameters)
+        if let head = headers {
+            for header in head {
+                result.addValue(header.value, forHTTPHeaderField: header.name)
+            }
+        }
+        return result
+    }
+}
