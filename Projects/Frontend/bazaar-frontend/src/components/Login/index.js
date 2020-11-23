f4d940ee4eaf1630239d6c267006aa5020fc5d88
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-11-23 21:08:46 +0300

    post request

--- Projects/Frontend/bazaar-frontend/src/components/Login/index.js
+++ Projects/Frontend/bazaar-frontend/src/components/Login/index.js
@@ -1,98 +1,85 @@
 import React, { Component } from "react";
 import GoogleButton from 'react-google-button'
 import axios from 'axios'
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 
 import "./login.css";
 
 export default class LoginComponent extends Component {
 
-    constructor (){
-        super();
-        this.state = {
-            username: '',
-            password: '',
-        }
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
     }
+  }
 
 
-    login (event){
-        const data = new FormData();
+  handleChange = event => {
 
-        data.append("username", this.state.username);
-        data.append("password", this.state.password);
-        console.log(" data: "+data)
+    this.setState({ [event.target.name]: event.target.value });
 
-        axios.post('http://13.59.236.175:8000/api/user/login/', data)
-        .then(response => {
-            console.log(response.status)
-            response.JSON()
-            .then(res => {
-                console.log(res);
-                console.log(res.status);
+  }
 
-            })
-            .catch(error =>{
-                if (error.response){
-                    if (error.response.status == 401){
-                        alert ("The email or password you have entered is incorrect. Please try again.");
-                    }
-                    else{
-                        alert ("There has been an error. Please try again.");
-                    }
-                }
-                else{
-                    alert ("There has been an error. Please try again.");
-                }
+  handleSubmit = event => {
 
-            })
-        event.preventDefault();
-    })
-}
+    const cookies = new Cookies();
+    event.preventDefault();
 
-      onChange = (e) => {
+    // const data = { "username": "omerBenzer61@bazaar.com", "password": "mypw" }
 
-        this.setState({ [e.target.name]: e.target.value });
-      }
+
+    axios.post(`http://13.59.236.175:8000/api/user/login/`, { "username": this.state.username, "password": this.state.password })
+      .then(res => {
+
+        const cookie_key = 'user';
+        bake_cookie(cookie_key, res);
+
+        console.log(res);
+        console.log(res.data);
+      })
 
 
 
+  }
 
 
-    render() {
-        return (
-            <div className="entry-form">
-                <form onSubmit={this.login}>
-                    <h3>Sign In</h3>
+  render() {
+    return (
+      <div className="entry-form">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Sign In</h3>
 
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input type="text" name="username" className="form-control" placeholder="Enter email" 
-                        onChange={this.onchange}/>
-                    </div>
+          <div className="form-group">
+            <label>Email address</label>
+            <input type="text" name="username" className="form-control" placeholder="Enter email"
+              onChange={this.handleChange} />
+          </div>
 
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input type="text" name="password" className="form-control" placeholder="Enter password" 
-                        onChange={this.onchange}/>
-                    </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="text" name="password" className="form-control" placeholder="Enter password"
+              onChange={this.handleChange} />
+          </div>
 
-                    <div className="form-group">
-                        <div className="custom-control custom-checkbox">
-                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                        </div>
-                    </div>
-
-                    <button id="submit" type="submit" className="btn btn-block">Sign in</button>
-                    <p className="forgot-password">
-                        Forgot <a href="#">password?</a>
-                    </p>
-                </form>
-                <GoogleButton className="btn-google"
-                    onClick={() => { console.log('Google button clicked') }}
-                /> 
+          <div className="form-group">
+            <div className="custom-control custom-checkbox">
+              <input type="checkbox" className="custom-control-input" id="customCheck1" />
+              <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
             </div>
+          </div>
 
-        );
-    }
+          <button id="submit" type="submit" className="btn btn-block">Sign in</button>
+          <p className="forgot-password">
+            Forgot <a href="#">password?</a>
+          </p>
+        </form>
+        <GoogleButton className="btn-google"
+          onClick={() => { console.log('Google button clicked') }}
+        />
+      </div>
+
+    );
+  }
 }
