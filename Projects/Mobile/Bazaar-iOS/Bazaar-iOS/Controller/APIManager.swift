b92236a6646ba61892a3fbb298b0166ad7374b4f
Muhsin Etki <44210081+muhsinetki@users.noreply.github.com>
2020-11-23 20:20:39 +0300

    Add APIManager file

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/APIManager.swift
@@ -0,0 +1,38 @@
+//
+//  APIManager.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 23.11.2020.
+//
+
+import UIKit
+import Alamofire
+
+struct APIManager {
+    let dateFormatter = DateFormatter()
+    
+    enum MyError: Error {
+        case runtimeError(String)
+    }
+    
+    func authenticate(username:String,password:String,completionHandler: @escaping (Result<String ,Error>) -> Void) {
+        do {
+            let request = try ApiRouter.authenticate(username: username, password: password).asURLRequest()
+            AF.request(request).responseJSON { (response) in
+                if (response.response?.statusCode != nil){
+                    guard let safeData = response.data else  {
+                        completionHandler(.failure(response.error!))
+                        return
+                    }
+                    if let decodedData:AuthData = APIParse().parseJSON(safeData: safeData){
+                        completionHandler(.success(decodedData.token))
+                    }else {
+                        completionHandler(.failure(MyError.runtimeError("Error")))
+                    }
+                }
+            }
+        }catch let err {
+            completionHandler(.failure(err))
+        }
+    }
+}
