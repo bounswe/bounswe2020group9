52cbe5df8abb4bd702ed00828bd08e987904af6a
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-11-25 17:01:24 +0300

    error handling
    detailed error description for each request

--- Projects/Backend/src/user/serializers.py
+++ Projects/Backend/src/user/serializers.py
@@ -6,7 +6,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('id',  'email', 'first_name', 'last_name', 'date_joined', 'last_login', 'user_type', 'bazaar_point')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
