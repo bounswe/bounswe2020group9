f0f7f217bd3ce0a4f0f0338355dd32f0c47f42f2
Mehmet Berk Kemaloğlu <berk.kemaloglu@boun.edu.tr>
2020-12-01 06:45:32 +0300

    Location API is finished.
    Token authorization is added. Multiple locations can be posted by 1 user. User can only view update or delete her/his locations.

--- Projects/Backend/src/location/urls.py
+++ Projects/Backend/src/location/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import LocationDetailAPIView, LocationListAPIView
+from .views import LocationDetailAPIView, LocationListAPIView, UserLocationDetailAPIView, UserLocationListAPIView
 
 urlpatterns = [
     path('', LocationListAPIView.as_view(), name="location-list"),
     path('<int:id>/', LocationDetailAPIView.as_view(), name="location-detail"),
+    path('byuser/<int:id>/', UserLocationDetailAPIView.as_view(), name="location-byuser"),
+    path('byuser/', UserLocationListAPIView.as_view(), name="location-byuser"),
 ]
--- Projects/Backend/src/location/views.py
+++ Projects/Backend/src/location/views.py
@@ -1,6 +1,9 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 from .models import Location
 from .serializers import LocationSerializer
@@ -50,3 +53,85 @@ class LocationDetailAPIView(APIView):
         location = self.get_location(id)
         location.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserLocationListAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            user_id = request.user.id
+        except:
+            return Response({"message": "Token is not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            locations = Location.objects.filter(user=user_id)
+        except:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        parent = LocationDetailAPIView()
+        serializers = []
+        for location in locations:
+            location = parent.get_location(location.id)
+            serializers.append(LocationSerializer(location).data)
+        
+        return Response(serializers)
+
+    def post(self, request):
+        try:
+            user_id = request.user.id
+        except:
+            return Response({"message": "Token is not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = LocationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            if serializer.validated_data['user'].id == user_id:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "Token and user id didn't match"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserLocationDetailAPIView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, id):
+        try:
+            user_id = request.user.id
+        except:
+            return Response({"message": "Token is not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = LocationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            if serializer.validated_data['user'].id == user_id:
+                parent = LocationDetailAPIView()
+                return parent.put(request, id)
+            else:
+                return Response({"message": "Token and user id didn't match"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def delete(self, request, id):
+        try:
+            user_id = request.user.id
+        except:
+            return Response({"message": "Token is not valid."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            location = Location.objects.get(id=id)
+
+        except Location.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if location.user.id == user_id:
+            location.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "Token and user id didn't match"}, status=status.HTTP_401_UNAUTHORIZED) 
+
