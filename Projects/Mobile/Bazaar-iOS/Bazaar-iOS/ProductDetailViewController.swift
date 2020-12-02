7f752be0a304f869d55c8f285028683793cfa031
Muhsin Etki <muhsinetki@boniglobal.com>
2020-12-02 19:11:44 +0300

    Create product detail view controller

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/ProductDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProductDetailViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 2.12.2020.
+//
+
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
