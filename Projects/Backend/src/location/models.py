db21137f7e301d24598c65d63292fa31a5514dc1
Mehmet Berk Kemaloğlu <berk.kemaloglu@boun.edu.tr>
2020-12-04 01:49:30 +0300

    2 new fields are added to support google maps
    Google maps widget on mobşile and web uses 2 input: Longitude and latitude. So, I added them to location model.

--- Projects/Backend/src/location/models.py
+++ Projects/Backend/src/location/models.py
@@ -8,4 +8,6 @@ class Location(models.Model):
     address_name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     postal_code = models.IntegerField()
+    longitude = models.FloatField(default=0)
+    latitude = models.FloatField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
