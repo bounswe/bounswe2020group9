f31ca8a56137d45d3f2d06747cd7a2718721f99a
Muhsin Etki <muhsinetki@boniglobal.com>
2020-12-12 00:19:44 +0300

    Delete unusable codes in ProfileViewController

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/ProfileViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/ProfileViewController.swift
@@ -10,18 +10,12 @@ import GoogleSignIn
 
 class ProfileViewController: UIViewController{
     
-    @IBOutlet weak var profileMenu: UITableView!
     @IBOutlet weak var userEmailLabel: UILabel!
-    @IBOutlet weak var userAddressLabel: UILabel!
-    @IBOutlet weak var userNameLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        profileMenu.delegate = self
-        profileMenu.dataSource = self
-        profileMenu.layer.cornerRadius = 10
-        profileMenu.layer.borderColor = UIColor.orange.cgColor
-        profileMenu.layer.borderWidth = 0.5
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,22 +31,8 @@ class ProfileViewController: UIViewController{
             self.view.isHidden = true
             performSegue(withIdentifier: "ProfileToLoginSegue", sender: self)
         }
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        if let selectionIndexPath = profileMenu.indexPathForSelectedRow {
-                profileMenu.deselectRow(at: selectionIndexPath, animated: false)
-            }
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidLayoutSubviews()
-        let topInset = max((profileMenu.frame.height - profileMenu.contentSize.height) / 2.0, 0.0)
-        profileMenu.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
-    }
     
     @IBAction func temporaryLogoutButton(_ sender: UIButton) {
         UserDefaults.standard.set(false, forKey: K.isLoggedinKey)
@@ -73,27 +53,6 @@ class ProfileViewController: UIViewController{
     }
 }
 
-//MARK: - UITableViewDelegate,UITableViewDataSource
-extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =  tableView.dequeueReusableCell(withIdentifier: "Menu Cell \(indexPath.row)", for: indexPath)
-        let selectedView = UIView()
-        selectedView.backgroundColor = #colorLiteral(red: 0.9402856827, green: 0.6186184287, blue: 0.1447118819, alpha: 0.4013809419)
-        selectedView.layer.cornerRadius = 10
-        cell.selectedBackgroundView = selectedView
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return profileMenu.frame.height / 7
-    }
-    
-}
 //MARK: - Extension LoginViewControllerDelegate
 extension ProfileViewController: LoginViewControllerDelegate{
     func loginViewControllerDidPressSignUp(isPressed: Bool) {
