8b2831f7ee8e5b2c4b71df8f4b9c5085753a3f23
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2020-12-13 14:07:01 +0300

    alternative API call: /api/product/vendor/<id>/

--- Projects/Backend/src/product/urls.py
+++ Projects/Backend/src/product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from user.views import vendorProductListView
 from .views import ProductDetailAPIView, ProductListAPIView, UserCommentAPIView, CommentsOfProductAPIView, AddCommentAPIView, UpdateCommentAPIView
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     path('comment/<int:pid>/<int:uid>/', UserCommentAPIView.as_view(), name="user-comment"),
     path('comment/<int:pid>/', CommentsOfProductAPIView.as_view(), name="get-all-comments"),
     path('comment/update/<int:id>/', UpdateCommentAPIView.as_view(), name="update-comment"),
-    path('comment/', AddCommentAPIView.as_view(), name="add-comments"), 
+    path('comment/', AddCommentAPIView.as_view(), name="add-comments"),
+    path('vendor/<int:vendor_id>/', vendorProductListView.as_view(), name="vendor-products"),
 ]
