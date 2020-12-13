38752ccafdca47a4a3d990b58c7b6783c016d632
Muhsin Etki <muhsinetki@boniglobal.com>
2020-12-13 19:38:26 +0300

    Create MyAccountViewController

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/MyAccountViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MyAccountViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 13.12.2020.
+//
+
+import UIKit
+
+class MyAccountViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
