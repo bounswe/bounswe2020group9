8928368243099374d8187db0eeca621a56d4f198
ibrahimorhan <ibrahim.orhan@boun.edu.tr>
2020-12-21 17:48:25 +0300

    first and last name bug fixed

--- Projects/Backend/src/user/serializers.py
+++ Projects/Backend/src/user/serializers.py
@@ -10,11 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}, 'username': {'write_only': True}}
 
     def create(self, validated_data):
+          
         user = User(
             email=validated_data['username'],
             username=validated_data['username'],
             user_type=validated_data['user_type']
         )
+        if validated_data['first_name'] :
+            user.first_name = validated_data['first_name']
+        if validated_data['last_name'] :
+            user.last_name = validated_data['last_name']  
         user.set_password(validated_data['password'])
         user.save()
         return user
