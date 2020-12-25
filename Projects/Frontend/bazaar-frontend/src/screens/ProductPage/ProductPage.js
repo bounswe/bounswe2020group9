df5ea84109474062cc034b323e8f9214c3d7462d
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-12-25 19:43:22 +0300

    Carousel Name Changes

--- Projects/Frontend/bazaar-frontend/src/screens/ProductPage/ProductPage.js
+++ Projects/Frontend/bazaar-frontend/src/screens/ProductPage/ProductPage.js
@@ -33,10 +33,6 @@ export default class Productpage extends Component {
                     src={product.picture}
                     alt="First slide"
                   />
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
                   <img
@@ -44,10 +40,6 @@ export default class Productpage extends Component {
                     src={product.picture}
                     alt="Third slide"
                   />
-                  <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
@@ -55,10 +47,6 @@ export default class Productpage extends Component {
                     src={product.picture}
                     alt="Third slide"
                   />
-                  <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                  </Carousel.Caption>
                 </Carousel.Item>
               </Carousel>
             </Col>
