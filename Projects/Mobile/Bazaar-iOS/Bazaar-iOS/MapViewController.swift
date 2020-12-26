ae81aa1b7a880b2a0a00c1c7730a5f5e8267fa45
Muhsin Etki <muhsin.etki@boun.edu.tr>
2020-12-26 21:43:33 +0300

    Add MapViewController

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 26.12.2020.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
