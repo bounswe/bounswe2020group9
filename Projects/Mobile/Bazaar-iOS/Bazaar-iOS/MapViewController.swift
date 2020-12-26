d3d6f08e79df5f3654ac4306f1ae9c4ca002a753
Muhsin Etki <muhsin.etki@boun.edu.tr>
2020-12-26 22:14:29 +0300

    Get location info from map

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/MapViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/MapViewController.swift
@@ -8,8 +8,25 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController , MKMapViewDelegate, UIGestureRecognizerDelegate  {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        let gestureZ = UILongPressGestureRecognizer(target: self, action: #selector(self.revealRegionDetailsWithLongPressOnMap(sender:)))
+        view.addGestureRecognizer(gestureZ)
+    }
+    
+    @IBAction func revealRegionDetailsWithLongPressOnMap(sender: UILongPressGestureRecognizer) {
+        if sender.state != UIGestureRecognizer.State.began { return }
+        let touchLocation = sender.location(in: mapView)
+        
+        let locationCoordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locationCoordinate
+        mapView.addAnnotation(annotation)
+        print("Tapped at lat: \(locationCoordinate) long: \(locationCoordinate.longitude)")
     }
 }
