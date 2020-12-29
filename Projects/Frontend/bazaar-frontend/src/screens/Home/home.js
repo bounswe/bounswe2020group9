45312caa36c05127cd0247694ee790bfaf590e3e
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2020-12-29 11:15:37 +0300

    Update welcome message

--- Projects/Frontend/bazaar-frontend/src/screens/Home/home.js
+++ Projects/Frontend/bazaar-frontend/src/screens/Home/home.js
@@ -1,148 +1,145 @@
-import React, { Component } from 'react';
-import Pagination from 'react-bootstrap/Pagination'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Button from 'react-bootstrap/Button'
-import Image from 'react-bootstrap/Image'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import CardDeck from 'react-bootstrap/CardDeck'
-import axios from 'axios'
-import Card from "../../components/ProductCard/productCard"
-import {serverUrl} from '../../utils/get-url'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardDeck from "react-bootstrap/CardDeck";
+import axios from "axios";
+import Card from "../../components/ProductCard/productCard";
+import { serverUrl } from "../../utils/get-url";
+import { Link } from "react-router-dom";
 
-
-
-import './home.scss'
+import "./home.scss";
 
 class Home extends Component {
-
   constructor() {
     super();
     this.state = {
-      isLogged: 'yes',
+      isLogged: "yes",
       redirect: null,
       categoryList: [],
       categoryHidden: true,
       categoryDict: {},
-      categoryStructure: {'': []},
-      products: []
-    }
+      categoryStructure: { "": [] },
+      products: [],
+    };
   }
 
-
   componentDidMount() {
-    axios.get(serverUrl+`api/product/`)
-      .then(res => {
-        this.setState({ products: res.data })
-      })
-    axios.get(serverUrl+'api/product/categories/')
-    .then(res => {
+    axios.get(serverUrl + `api/product/`).then((res) => {
+      this.setState({ products: res.data });
+    });
+    axios.get(serverUrl + "api/product/categories/").then((res) => {
       let resp = res.data;
       let categoryStructureTemp = {};
       let keys = [];
-      let categoryDictTemp = {}
-      for (let i=0;i<resp.length;i++) {
+      let categoryDictTemp = {};
+      for (let i = 0; i < resp.length; i++) {
         if (resp[i]["parent"] == "Categories") {
-          keys.push(resp[i]["name"])
-          categoryDictTemp[resp[i]["id"]] = resp[i]["name"]
+          keys.push(resp[i]["name"]);
+          categoryDictTemp[resp[i]["id"]] = resp[i]["name"];
         }
       }
-      this.setState({categoryList: keys})
-      this.setState({categoryDict: categoryDictTemp})
-      for (let i=0;i<keys.length;i++) {
-        let sublist = []
-        for (let j=0;j<resp.length;j++) {
+      this.setState({ categoryList: keys });
+      this.setState({ categoryDict: categoryDictTemp });
+      for (let i = 0; i < keys.length; i++) {
+        let sublist = [];
+        for (let j = 0; j < resp.length; j++) {
           if (resp[j]["parent"] == keys[i]) {
             sublist.push(resp[j]["name"]);
           }
         }
         categoryStructureTemp[keys[i]] = sublist;
       }
-      categoryStructureTemp[''] = []
-      console.log("category strructure: "+categoryStructureTemp)
+      categoryStructureTemp[""] = [];
+      console.log("category strructure: " + categoryStructureTemp);
 
-      this.setState({categoryStructure: categoryStructureTemp})
-    })
+      this.setState({ categoryStructure: categoryStructureTemp });
+    });
   }
 
   render() {
     let active = 2;
-    let categoryList = this.state.categoryList
-    let categoryStructure = this.state.categoryStructure
+    let categoryList = this.state.categoryList;
+    let categoryStructure = this.state.categoryStructure;
     let categories = [];
     for (let number = 0; number < categoryList.length; number++) {
       let subs = [];
-      let subList = categoryStructure[categoryList[number]]
-      if (subList){
-        for (let subnumber = 0; subnumber < subList.length; subnumber++){
+      let subList = categoryStructure[categoryList[number]];
+      if (subList) {
+        for (let subnumber = 0; subnumber < subList.length; subnumber++) {
           subs.push(
-            <a className="dropdown-item" href={"/category/"+subList[subnumber]}>{subList[subnumber]}</a>
-          )
+            <a
+              className="dropdown-item"
+              href={"/category/" + subList[subnumber]}
+            >
+              {subList[subnumber]}
+            </a>
+          );
         }
       }
 
-
       categories.push(
-        <Pagination.Item key={number} className="myPaginationItem dropdown" href={"/category/"+categoryList[number]}>
-          <a className="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <Pagination.Item
+          key={number}
+          className="myPaginationItem dropdown"
+          href={"/category/" + categoryList[number]}
+        >
+          <a
+            className="nav-link dropdown-toggle"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
             <span className="mr-1"></span> {categoryList[number]}
           </a>
-          <div className="dropdown-menu">
-            {subs}
-          </div>
-          
+          <div className="dropdown-menu">{subs}</div>
         </Pagination.Item>
       );
     }
 
-
-    let categoryDict = this.state.categoryDict
-    let productCards = this.state.products.map(product => {
-      console.log(JSON.stringify(product))
-        return (
-          <Col sm="3">
-            <Card product={product}></Card>
-          </Col>
-        )
-    })
+    let categoryDict = this.state.categoryDict;
+    let productCards = this.state.products.map((product) => {
+      console.log(JSON.stringify(product));
+      return (
+        <Col sm="3">
+          <Card product={product}></Card>
+        </Col>
+      );
+    });
 
     return (
-
       <div>
-        <div className='homeWrapper'>
+        <div className="homeWrapper">
           <Container>
-            <div className='myPagination'>
+            <div className="myPagination">
               <Pagination size="lg">{categories}</Pagination>
             </div>
-            <div className='row homeJumbotron'>
-              <Jumbotron style={{background:"transparent"}}>
+            <div className="row homeJumbotron">
+              <Jumbotron style={{ background: "transparent" }}>
                 <h1>Welcome to Bazaar!</h1>
                 <p>
-                  "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
-                  demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee
-                  the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty
-                  through weakness of will, which is the same as saying through shrinking from toil and pain. These cases
-                  are perfectly simple and easy to distinguish
-              </p>
+                  Bazaar is a compatile, fast and user-friendly e-commerce
+                  Website. It's been developed by group of people who concern
+                  about good user experience and better design because we care
+                  people's eyes!
+                </p>
                 <p>
                   <Button variant="outline-info">Learn more</Button>
                 </p>
               </Jumbotron>
             </div>
             <Container fluid>
-              <Row>
-                {productCards}
-              </Row>
+              <Row>{productCards}</Row>
             </Container>
           </Container>
         </div>
       </div>
     );
-
   }
 }
-
 
 export default Home;
