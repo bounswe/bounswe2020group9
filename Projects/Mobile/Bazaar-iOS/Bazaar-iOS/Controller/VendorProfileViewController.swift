ac70e9a30a927679296954d3845ba995eb4ba4f5
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-17 22:59:39 +0300

    Desing vendor profile page

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/VendorProfileViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/VendorProfileViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class VendorProfileViewController: UIViewController {
+    
+    @IBOutlet weak var menuView: UIView!
+    
     override func viewDidLoad() {
+        menuView.layer.borderColor = #colorLiteral(red: 0.2549019608, green: 0.6823529412, blue: 0.662745098, alpha: 1)
+        menuView.layer.shadowColor = UIColor.black.cgColor
         super.viewDidLoad()
     }
 }
