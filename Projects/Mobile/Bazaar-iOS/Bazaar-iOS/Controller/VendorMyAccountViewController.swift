d1642ebc42b18956911a171e64162f0d919c6356
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-18 19:47:33 +0300

    Establish first baseline for vendor my account page

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/VendorMyAccountViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VendorMyAccountViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 18.01.2021.
+//
+
+import UIKit
+
+class VendorMyAccountViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var currentPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordAgainTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func updateButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func logoutButtonPressed(_ sender: UIButton) {
+    }
+}
