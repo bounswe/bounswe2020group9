9012bebd1fd76b13082c7bf78742a2e61e708292
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-20 22:49:54 +0300

    Implement the credit card cell

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/View/CreditCardCell.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/View/CreditCardCell.swift
@@ -9,4 +9,28 @@ import UIKit
 
 class CreditCardCell: UITableViewCell {
     
+    @IBOutlet weak var creditCardCellView: CreditCardCell!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    @IBOutlet weak var deadlineLabel: UILabel!
+    @IBOutlet weak var cvvLabel: UILabel!
+    @IBOutlet weak var cellDeleteButton: UIButton!
+    
+    
+    func setCreditCard(name:String , cardNumber:String, deadline:String, cvv:String, index:Int) -> CreditCardCell {
+        
+        self.nameLabel.text = name
+        self.cardNumberLabel.text = cardNumber
+        self.deadlineLabel.text = deadline
+        self.cvvLabel.text = cvv
+        self.cellDeleteButton.setTitle("\(index)", for: .normal)
+        self.creditCardCellView.layer.shadowColor = UIColor.black.cgColor
+        self.creditCardCellView.layer.shadowOpacity = 0.2
+        self.creditCardCellView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.creditCardCellView.layer.shadowRadius = 3
+        return self
+    }
+    
+    @IBAction func cellDeleteButtonPressed(_ sender: UIButton) {
+    }
 }
