f11de6f3262477f8e6091ed8027c0018b111daac
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-21 21:55:10 +0300

    Design Add new credit card page

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/AddCreditCardViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddCreditCardViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 21.01.2021.
+//
+
+import UIKit
+
+class AddCreditCardViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
