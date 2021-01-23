ac40f89d0746064d4f1c37f18d7c2cba25b2a2a7
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-23 19:42:21 +0300

    Design customer my addresses page

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/CustomerMyAddressesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerMyAddressesViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 23.01.2021.
+//
+
+import UIKit
+
+class CustomerMyAddressesViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
