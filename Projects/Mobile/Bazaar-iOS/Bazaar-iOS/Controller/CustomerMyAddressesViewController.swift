75b8844d9b9e292dec03a125c3ebf91922ff1087
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-23 21:13:38 +0300

    Implement AddressCell

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/CustomerMyAddressesViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/CustomerMyAddressesViewController.swift
@@ -17,6 +17,7 @@ class CustomerMyAddressesViewController: UIViewController {
         myAddressesTableView.layer.borderColor = #colorLiteral(red: 1, green: 0.6235294118, blue: 0, alpha: 1)
         self.myAddressesTableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
         self.myAddressesTableView.dataSource = self
+        myAddressesArray.append(AddressData(id: 1, address_name: "Address Name", address: "Karacaoglan Mah. 89. sok no:98", country: "Turkiye", city: "K.maras", postal_code: 46400, longitude: 1.11, latitude: 1.112))
     }
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
@@ -33,7 +34,7 @@ extension CustomerMyAddressesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "AddressCell", for: indexPath) as! AddressCell
         let addressData = self.myAddressesArray[indexPath.row]
-
+        cell = cell.setAddress(addressId: addressData.id, addressName: addressData.address_name, openAddress: addressData.address, country: addressData.country, city: addressData.city, postalCode: addressData.postal_code, latitude: addressData.latitude, longitude: addressData.longitude, user: 58)
         return cell
     }
 }
--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/View/AddressCell.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/View/AddressCell.swift
@@ -9,4 +9,38 @@ import UIKit
 
 class AddressCell: UITableViewCell {
     
+    var addressId:Int?
+    var user: Int?
+    var latitude:Float?
+    var longitude:Float?
+    
+    @IBOutlet weak var addressCellView: AddressCell!
+    @IBOutlet weak var addressNameLabel: UILabel!
+    @IBOutlet weak var countryNameLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var postalCodeLabel: UILabel!
+    @IBOutlet weak var openAddressLabel: UILabel!
+    
+    func setAddress(addressId:Int, addressName:String , openAddress:String, country:String, city:String, postalCode:Int,latitude:Float, longitude:Float, user:Int) -> AddressCell {
+        self.addressId = addressId
+        self.addressNameLabel.text = addressName
+        self.openAddressLabel.text = openAddress
+        self.countryNameLabel.text = country
+        self.cityNameLabel.text = city
+        self.postalCodeLabel.text = "\(postalCode)"
+        self.latitude = latitude
+        self.longitude = longitude
+        self.user = user
+        self.addressCellView.layer.shadowColor = UIColor.black.cgColor
+        self.addressCellView.layer.shadowOpacity = 0.2
+        self.addressCellView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        self.addressCellView.layer.shadowRadius = 3
+        return self
+    }
+    
+    @IBAction func updateButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func removeButtonPressed(_ sender: UIButton) {
+    }
 }
