054a799dc469a8ce934684b2f5d14091b3efd6d8
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-23 21:43:10 +0300

    Design AddNewAddressViewController

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/AddNewAddressViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddNewAddressViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 23.01.2021.
+//
+
+import UIKit
+
+class AddNewAddressViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
