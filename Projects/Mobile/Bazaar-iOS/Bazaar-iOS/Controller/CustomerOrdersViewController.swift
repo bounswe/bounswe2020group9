dbc71317ba6b977019c832db0aad04d7c988b99d
Sadi Uysal <sadiuysalsadi@gmail.com>
2021-01-23 16:30:38 +0300

    Order Cell design constraints updated, not completed, still have issues.

--- Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/CustomerOrdersViewController.swift
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/CustomerOrdersViewController.swift
@@ -11,7 +11,7 @@ class CustomerOrdersViewController: UIViewController{
     
 
     @IBOutlet weak var ordersTableView: UITableView!
-    var allOrdersInstance = AllOrders.shared
+    //var allOrdersInstance = AllOrders.shared
     var selectedCategoryName: String? //look again
     var orders: [Product] = []
     
@@ -41,17 +41,17 @@ class CustomerOrdersViewController: UIViewController{
         selectedCategoryName = "Clothing"
         ordersTableView.dataSource = self
         ordersTableView.delegate = self
-        allOrdersInstance.delegate = self
+        // allOrdersInstance.delegate = self
         let okButton = UIAlertAction(title: "Retry", style: .cancel, handler: { action in
             // fetch orders
-            self.allOrdersInstance.fetchAllOrders()
+            //self.allOrdersInstance.fetchAllOrders()
         })
         networkFailedAlert.addAction(okButton)
-        ordersTableView.register(UINib(nibName: "ProductCell", bundle: nil), forCellReuseIdentifier: "ReusableProdcutCell")
-        if !(allOrdersInstance.dataFetched) {
+        ordersTableView.register(UINib(nibName: "OrderCell", bundle: nil), forCellReuseIdentifier: "ReusableOrderCell")
+        /*if !(allOrdersInstance.dataFetched) {
             //startIndicator()
             self.allOrdersInstance.fetchAllOrders()
-        }
+        }*/
         
     }
     
@@ -96,17 +96,17 @@ class CustomerOrdersViewController: UIViewController{
 extension CustomerOrdersViewController:UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return 10
-        if tableView == ordersTableView {
+        /*if tableView == ordersTableView {
             return allOrdersInstance.All_Orders.filter{($0.category.parent?.contains(selectedCategoryName!))! || $0.category.name.contains(selectedCategoryName!)}.count
-        }
+        }*/
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = ordersTableView.dequeueReusableCell(withIdentifier: "ReusableProdcutCell", for: indexPath) as! ProductCell
-        cell.productImageView?.image = UIImage(named:"xmark.circle")
-        let filteredorders:[ProductData] = allOrdersInstance.All_Orders.filter{($0.category.parent?.contains(selectedCategoryName!))! || $0.category.name.contains(selectedCategoryName!)}
-        let product = filteredorders[indexPath.row]
+        let cell = ordersTableView.dequeueReusableCell(withIdentifier: "ReusableOrderCell", for: indexPath) as! OrderCell
+        cell.ProductImage?.image = UIImage(named:"xmark.circle")
+        /*let filteredorders:[ProductData] = allOrdersInstance.All_Orders.filter{($0.category.parent?.contains(selectedCategoryName!))! || $0.category.name.contains(selectedCategoryName!)}*/
+        /*let product = filteredorders[indexPath.row]
         cell.productNameLabel.text = product.name
         cell.productNameLabel.font = UIFont.systemFont(ofSize: 15, weight: .black)
         cell.productDescriptionLabel.text = product.brand
@@ -131,15 +131,15 @@ extension CustomerOrdersViewController:UITableViewDelegate,UITableViewDataSource
                     cell.productImageView.contentMode = .center
                 }
             }
-        }
+        }*/
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let filteredorders:[ProductData] = allOrdersInstance.All_Orders.filter{($0.category.parent?.contains(selectedCategoryName!))! || $0.category.name.contains(selectedCategoryName!)}
+        /*let filteredorders:[ProductData] = allOrdersInstance.All_Orders.filter{($0.category.parent?.contains(selectedCategoryName!))! || $0.category.name.contains(selectedCategoryName!)}
         let product = filteredorders[indexPath.row]
-        print(product.name)
+        print(product.name)*/
         performSegue(withIdentifier: "mainToProductDetailSegue", sender: nil)
         
     }
@@ -148,7 +148,7 @@ extension CustomerOrdersViewController:UITableViewDelegate,UITableViewDataSource
 
 
 
-
+/*
 extension CustomerOrdersViewController: AllOrdersFetchDelegate {
     func AllOrdersAreFetched() {
         self.stopIndicator()
@@ -174,7 +174,7 @@ extension CustomerOrdersViewController: AllOrdersFetchDelegate {
     }
 }
 
-
+*/
 
 
 // MARK: - IndicatorView
@@ -207,82 +207,11 @@ extension CustomerOrdersViewController {
     }
 }
 
-protocol AllOrdersFetchDelegate {
+/*
+ protocol AllOrdersFetchDelegate {
     func AllOrdersAreFetched()
     func ordersCannotBeFetched()
     func presentAlert()
-}
+}*/
 
-class AllOrders {
-    static let shared = AllOrders()
-    var All_Orders: [ProductData]
-    var allImages: Dictionary<Int, UIImage>
-    var allImageNames: [String]
-    private let saveKey = "AllOrders"
-    
-    var delegate: AllOrdersFetchDelegate?
-    let dispatchGroup = DispatchGroup()
-    var dataFetched = false {
-        didSet{
-            if self.dataFetched{
-                delegate?.AllOrdersAreFetched()
-            } else {
-                delegate?.ordersCannotBeFetched()
-            }
-        }
-    }
-    var apiFetchError = false
-    var jsonParseError = false
-    
-    init(){
-        self.All_Orders = []
-        self.allImages = Dictionary()
-        self.allImageNames = []
-    }
-    
-    func fetchAllOrders() {
-        dispatchGroup.enter()
-        APIManager().getAllProducts(completionHandler: { orders in
-            if orders != nil {
-                self.dataFetched = true
-                self.All_Orders = orders!
-                let group = DispatchGroup()
-                let serialQueue = DispatchQueue(label: "serialQueue")
-                for prod in self.All_Orders {
-                    group.enter()
-                    if let pic = prod.picture {
-                        print(prod.name, pic)
-                        let url = URL(string: prod.picture!)
-                        URLSession(configuration: .default).dataTask(with: url!) { (data, response, error) in
-                            guard let data = data, let image = UIImage(data: data), error == nil else { group.leave(); return }
-                            
-                            // ***************************************************************************
-                            // creates a synchronized access to the images array
-                            serialQueue.async {
-                                self.allImages[prod.id] = image
-                                
-                                // ****************************************************
-                                // tells the group a pending process has been completed
-                                print(prod.id, "done")
-                                group.leave()
-                            }
-                        }.resume()
-                    } else {
-                        group.leave()
-                    }
-                    
-                }
-                group.wait()
-                self.delegate?.AllOrdersAreFetched()
-            } else {
-                self.dataFetched = false
-                self.All_Orders = []
-                self.delegate?.ordersCannotBeFetched()
-            }
-        })
-        dispatchGroup.leave()
-        dispatchGroup.wait()
-    }
-        
-}
 
