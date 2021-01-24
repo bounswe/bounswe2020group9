5cc673cd5c6faf9fe42c273db5fa024a76c53fef
Muhsin Etki <muhsin.etki@boun.edu.tr>
2021-01-24 15:48:00 +0300

    Design my addresses page for vendor

---
+++ Projects/Mobile/Bazaar-iOS/Bazaar-iOS/Controller/VendorMyAddressesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  VendorMyAddressesViewController.swift
+//  Bazaar-iOS
+//
+//  Created by Muhsin Etki on 24.01.2021.
+//
+
+import UIKit
+
+class VendorMyAddressesViewController: UIViewController {
+    
+    @IBOutlet weak var myAddressesTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func addNewAddressButtonPressed(_ sender: UIButton) {
+    }
+}
