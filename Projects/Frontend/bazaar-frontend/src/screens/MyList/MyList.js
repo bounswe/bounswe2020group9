fc365d24ba62a5b656b290742a0339bbeb63008b
hasandemirkiran <43812957+hasandemirkiran@users.noreply.github.com>
2021-01-24 22:59:53 +0300

    List Add Row added

--- Projects/Frontend/bazaar-frontend/src/screens/MyList/MyList.js
+++ Projects/Frontend/bazaar-frontend/src/screens/MyList/MyList.js
@@ -12,6 +12,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ListGroup from "react-bootstrap/ListGroup";
 import CategoryBar from "../../components/category-bar/category-bar";
+import Form from "react-bootstrap/Form";
 
 //helpers
 import { serverUrl } from "../../utils/get-url";
@@ -19,6 +20,7 @@ import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
 
 //icons
 import addListIcon from "../../assets/icons/add-list-icon.svg";
+import removeListIcon from "../../assets/icons/remove.svg";
 
 export default class MyList extends Component {
   constructor(props) {
@@ -27,6 +29,8 @@ export default class MyList extends Component {
       productLists: [],
       whichList: 1,
       productList: {},
+      list_name: "",
+      list_is_private: false,
     };
   }
 
@@ -79,7 +83,6 @@ export default class MyList extends Component {
   }
 
   renderList() {
-    console.log(this.state.productList, "product list");
     let productCards = this.state.productList.products?.map((product) => {
       return (
         <Col sm="3">
@@ -91,15 +94,63 @@ export default class MyList extends Component {
     return <Row>{productCards}</Row>;
   }
 
+  onAddListSubmit = () => {
+    let myCookie = read_cookie("user");
+
+    const headers = {
+      Authorization: `Token ${myCookie.token}`,
+    };
+
+    const data = {
+      name: this.state.list_name,
+      customer: myCookie.user_id,
+      is_private: this.state.list_is_private,
+    };
+
+    axios
+      .post(serverUrl + `api/user/${myCookie.user_id}/lists/`, data, {
+        headers: headers,
+      })
+      .then((res) => {
+        axios
+          .get(serverUrl + `api/user/${myCookie.user_id}/lists/`, {
+            headers: {
+              Authorization: `Token ${myCookie.token}`,
+            },
+          })
+          .then((res) => {
+            this.setState({ productLists: res.data });
+          });
+      });
+  };
+
+  onListNameChange = (event) => {
+    this.setState({ list_name: event.target.value });
+    console.log(this.state.list_name);
+  };
+
+  onListPrivateChange = (event) => {
+    // this is slightly diffent from normal event.value, in this library check can be used by target.check value
+    this.setState({ list_is_private: event.target.checked });
+  };
+
   render() {
     let listNames = this.state.productLists?.map((list) => {
       return (
-        <button
-          className={"listButton"}
-          onClick={() => this.setState({ whichList: list.id })}
-        >
-          <ListGroup.Item>{list.name}</ListGroup.Item>
-        </button>
+        <Row className="listRow">
+          <button
+            className={"listButton"}
+            onClick={() => this.setState({ whichList: list.id })}
+          >
+            <ListGroup.Item>{list.name}</ListGroup.Item>
+          </button>
+          <button
+            className="listRemoveButton"
+            onClick={this.onDeleteListButton}
+          >
+            <img src={removeListIcon} />
+          </button>
+        </Row>
       );
     });
 
@@ -110,11 +161,31 @@ export default class MyList extends Component {
           <Col xs={3} className={"lists"}>
             <Row>
               <h2>My Lists</h2>
-              <button className="addListButton">
+            </Row>
+            <ListGroup variant="flush">{listNames}</ListGroup>
+            <Row className="addListRow">
+              <Form.Group>
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  placeholder="Write new list name"
+                  onChange={this.onListNameChange}
+                />
+
+                <div className="isFormPrivate">
+                  <Form.Check
+                    type="switch"
+                    id="custom-switch"
+                    label="Private"
+                    onChange={this.onListPrivateChange}
+                  />
+                </div>
+              </Form.Group>
+
+              <button className="addListButton" onClick={this.onAddListSubmit}>
                 Add List <img src={addListIcon} />
               </button>
             </Row>
-            <ListGroup variant="flush">{listNames}</ListGroup>
           </Col>
           <Col xs={9}>{this.renderList()}</Col>
         </Row>
