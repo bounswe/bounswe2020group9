cc23096f4d85eaaccc937b2a079b5aaa22003639
firatbulut19 <44057526+firatbulut19@users.noreply.github.com>
2021-01-26 04:14:53 +0300

    product update now working.

--- Projects/Frontend/bazaar-frontend/src/screens/Inventory/inventory.js
+++ Projects/Frontend/bazaar-frontend/src/screens/Inventory/inventory.js
@@ -4,7 +4,7 @@ import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
 import Cookies from 'js-cookie';
 import DataTable from 'react-data-table-component';
 import { Modal, Button, Alert } from "react-bootstrap";
-import {serverUrl} from '../../utils/get-url'
+import { serverUrl } from '../../utils/get-url'
 
 import "./inventory.scss";
 
@@ -29,7 +29,7 @@ export default class Inventory extends Component {
       isHiddenSuccess: true,
       isHiddenDeleteSuccess: true,
       isHiddenDeleteFail: true,
-      categoryList: {"":[]}
+      categoryList: { "": [] }
     }
     this.handleImageChange = this.handleImageChange.bind(this)
 
@@ -38,30 +38,30 @@ export default class Inventory extends Component {
 
   componentDidMount() {
     let myCookie = read_cookie('user')
-    axios.get(serverUrl+'api/product/categories/')
-    .then(res => {
-      let resp = res.data;
-      let categoryListTemp = {};
-      let keys = [];
-      for (let i=0;i<resp.length;i++) {
-        if (resp[i]["parent"] == "Categories") {
-          keys.push(resp[i]["name"])
-        }
-      }
-      for (let i=0;i<keys.length;i++) {
-        let sublist = []
-        for (let j=0;j<resp.length;j++) {
-          if (resp[j]["parent"] == keys[i]) {
-            sublist.push(resp[j]["name"]);
+    axios.get(serverUrl + 'api/product/categories/')
+      .then(res => {
+        let resp = res.data;
+        let categoryListTemp = {};
+        let keys = [];
+        for (let i = 0; i < resp.length; i++) {
+          if (resp[i]["parent"] == "Categories") {
+            keys.push(resp[i]["name"])
           }
         }
-        categoryListTemp[keys[i]] = sublist;
-      }
-      categoryListTemp[''] = []
-      this.setState({categoryList: categoryListTemp})
-      
-    })
-    axios.get(serverUrl+`api/product/`)
+        for (let i = 0; i < keys.length; i++) {
+          let sublist = []
+          for (let j = 0; j < resp.length; j++) {
+            if (resp[j]["parent"] == keys[i]) {
+              sublist.push(resp[j]["name"]);
+            }
+          }
+          categoryListTemp[keys[i]] = sublist;
+        }
+        categoryListTemp[''] = []
+        this.setState({ categoryList: categoryListTemp })
+
+      })
+    axios.get(serverUrl + `api/product/`)
       .then(res => {
         let myProducts = res.data.filter(product => product.vendor === myCookie.user_id)
         this.setState({ products: myProducts })
@@ -71,91 +71,97 @@ export default class Inventory extends Component {
 
   }
 
-  handleValidation(){
+  handleValidation() {
     let formIsValid = true;
     let new_errors = {};
     //Name
-    if (this.state.name === ''){
+    if (this.state.name === '') {
       formIsValid = false;
       new_errors["name"] = "Name can not be empty.";
     }
-    if (this.state.brand === ''){
+    if (this.state.brand === '') {
       formIsValid = false;
       new_errors["brand"] = "Brand can not be empty.";
     }
-    if (this.state.price === ''){
+    if (this.state.price === '') {
       formIsValid = false;
       new_errors["price"] = "Price can not be empty.";
     }
-    if (this.state.stock === ''){
+    if (this.state.stock === '') {
       formIsValid = false;
       new_errors["stock"] = "Stock can not be empty.";
     }
-    if (this.state.category === ''){
+    if (this.state.category === '') {
       formIsValid = false;
       new_errors["category"] = "Category can not be empty.";
     }
 
 
 
-    this.setState({errors: new_errors});
+    this.setState({ errors: new_errors });
     return formIsValid;
   }
 
-
-  handleSubmit = event => {  
+  handleSubmit = event => {
     console.log("im at handlesubmit")
     event.preventDefault();
-      
+
     let myCookie = read_cookie('user');
-    const header = {headers: {Authorization: "Token "+myCookie.token}};
+    const header = { headers: { Authorization: "Token " + myCookie.token } };
     console.log(header)
+    console.log("event:  ", event)
 
     if (!this.state.delete) {
-      const body = new FormData();
-      body.append("name", this.state.name);
-      body.append("detail", this.state.detail);
-      body.append("brand", this.state.brand);
-      body.append("price", this.state.price);
-      body.append("stock", this.state.stock);
-      body.append("picture", this.state.image);
-      console.log("state image: "+this.state.image)
-      console.log(body)
+
+      const data = {
+        name: this.state.name,
+        detail: this.state.detail,
+        brand: this.state.brand,
+        price: this.state.price,
+        stock: this.state.stock
+      }
+      //let uploadedImage = URL.createObjectURL(event.target.files[0])
 
       if (this.handleValidation()) {
-          axios.put(serverUrl+`api/product/`+this.state.id+"/", body, header)
+        axios.put(serverUrl + `api/product/` + this.state.id + "/", data, header)
           .then(res => {
-    
+
             console.log("res: " + res);
-            console.log("res.data: "+res.data);
-            this.setState({isHiddenSuccess: false})
-  
+            console.log("res.data: " + res.data);
+            this.setState({ isHiddenSuccess: false })
+
+            axios.get(serverUrl + `api/product/`)
+              .then(res => {
+                let myProducts = res.data.filter(product => product.vendor === myCookie.user_id)
+                this.setState({ products: myProducts })
+              })
+
           }).catch(error => {
-            console.log("error: "+JSON.stringify(error))
-            this.setState({isHiddenFail: false})
-  
+            console.log("error: " + JSON.stringify(error))
+            this.setState({ isHiddenFail: false })
+
           })
-  
+
       } else {
-          this.setState({ [this.state.hasError]: true });
+        this.setState({ [this.state.hasError]: true });
       }
     } else {
-        axios.delete(serverUrl+`api/product/`+this.state.id+"/", header)
+      axios.delete(serverUrl + `api/product/` + this.state.id + "/", header)
         .then(res => {
-  
+
           console.log("res: " + res);
-          console.log("res.data: "+res.data);
-          this.setState({isHiddenDeleteSuccess: false})
+          console.log("res.data: " + res.data);
+          this.setState({ isHiddenDeleteSuccess: false })
           let myProducts = this.state.products.filter(product => product.id !== this.state.id)
-          this.setState({products: myProducts})
+          this.setState({ products: myProducts })
           this.closeModal()
 
         }).catch(error => {
-          console.log("error: "+JSON.stringify(error))
-          this.setState({isHiddenDeleteFail: false})
+          console.log("error: " + JSON.stringify(error))
+          this.setState({ isHiddenDeleteFail: false })
 
         })
-        this.setState({delete: false})
+      this.setState({ delete: false })
     }
 
   }
@@ -170,38 +176,37 @@ export default class Inventory extends Component {
 
     event.preventDefault();
     let uploadedImage = URL.createObjectURL(event.target.files[0])
-    this.setState({ image:  uploadedImage })
-    console.log("image: "+ uploadedImage.substring(5))
+    this.setState({ image: uploadedImage })
+    console.log("image: " + uploadedImage.substring(5))
 
   }
 
   rowClicked = (event) => {
     console.log(event)
-    this.setState({name: event.name})
-    this.setState({id: event.id})
-    this.setState({brand: event.brand})
-    this.setState({price: event.price})
-    this.setState({stock: event.stock})
-    this.setState({detail: event.detail})
-    this.setState({category: event.category["parent"]})
-    this.setState({subcategory: event.category["name"]})
-    this.setState({image: event.picture})
+    this.setState({ name: event.name })
+    this.setState({ id: event.id })
+    this.setState({ brand: event.brand })
+    this.setState({ price: event.price })
+    this.setState({ stock: event.stock })
+    this.setState({ detail: event.detail })
+    this.setState({ category: event.category["parent"] })
+    this.setState({ subcategory: event.category["name"] })
+    this.setState({ image: event.picture })
     this.openModal()
-    console.log("category: "+event.category["parent"])
-    console.log("subcategory: "+event.category["name"])
-    console.log("event: "+JSON.stringify(event))
+    console.log("category: " + event.category["parent"])
+    console.log("subcategory: " + event.category["name"])
   }
 
   deleteProduct = () => this.setState({ delete: true });
   openModal = () => {
     this.setState({ isOpen: true });
-    this.setState({isHiddenDeleteSuccess: true})
+    this.setState({ isHiddenDeleteSuccess: true })
   }
   closeModal = () => {
     this.setState({ isOpen: false })
-    this.setState({isHiddenFail: true})
-    this.setState({isHiddenSuccess: true})
-    this.setState({isHiddenDeleteFail: true})
+    this.setState({ isHiddenFail: true })
+    this.setState({ isHiddenSuccess: true })
+    this.setState({ isHiddenDeleteFail: true })
   };
 
   upload = () => {
@@ -236,7 +241,7 @@ export default class Inventory extends Component {
     ];
 
     let categories = Object.keys(this.state.categoryList).map(category => {
-      if (category !== ""){
+      if (category !== "") {
         return (
           <option value={category}>{category}</option>
         )
@@ -259,13 +264,13 @@ export default class Inventory extends Component {
           <form className='needs-validation' onSubmit={this.handleSubmit} noValidate>
 
             <Modal.Body>
-              
+
 
               <div className="col">
-                <div id="image-container">
-                  <img id="product-image" src={this.state.image} onClick={this.upload}/>
+                <div className="image-container">
+                  <img className="product-image" src={this.state.image} onClick={this.upload} />
                   <input id="select-image" type="file" name="image" className="" accept="image/jpeg image/png"
-                  onChange={this.handleImageChange}hidden/>
+                    onChange={this.handleImageChange} hidden />
                 </div>
                 <div className="error">{this.state.errors["image"]}</div>
 
@@ -280,75 +285,75 @@ export default class Inventory extends Component {
                 Something went wrong.
               </Alert>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Name:</label>
-                  <div className="col-6">
-                    <input type="text" name="name"className="form-control col" value={this.state.name}
-                    onChange={this.handleChange} required/>
-                    <div className="error">{this.state.errors["name"]}</div>
-                  </div>
+                <label className="col-4 align-middle">Name:</label>
+                <div className="col-6">
+                  <input type="text" name="name" className="form-control col" value={this.state.name}
+                    onChange={this.handleChange} required />
+                  <div className="error">{this.state.errors["name"]}</div>
+                </div>
               </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Detail:</label>
-                  <div className="col-6">
-                    <input type="text" name="detail" className="form-control col" value={this.state.detail}
-                    onChange={this.handleChange}/>
-                  </div>
+                <label className="col-4 align-middle">Detail:</label>
+                <div className="col-6">
+                  <input type="text" name="detail" className="form-control col" value={this.state.detail}
+                    onChange={this.handleChange} />
+                </div>
               </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Brand:</label>
-                  <div className="col-4">
-                    <input type="text" name="brand" className="form-control col" value={this.state.brand}
-                    onChange={this.handleChange} required/>
-                    <div className="error">{this.state.errors["brand"]}</div>
+                <label className="col-4 align-middle">Brand:</label>
+                <div className="col-4">
+                  <input type="text" name="brand" className="form-control col" value={this.state.brand}
+                    onChange={this.handleChange} required />
+                  <div className="error">{this.state.errors["brand"]}</div>
 
-                  </div>
+                </div>
               </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Category:</label>
-                  <div className="col-4">
-                    <select className="form-control col" name="category" id="category1" defaultValue={this.state.category}
+                <label className="col-4 align-middle">Category:</label>
+                <div className="col-4">
+                  <select className="form-control col" name="category" id="category1" defaultValue={this.state.category}
                     onChange={this.handleChange} required>
-                      <option disabled>Please Select</option>
-                      {categories}
-                    </select>
-                    <div className="error">{this.state.errors["category"]}</div>
-                  </div>
+                    <option disabled>Please Select</option>
+                    {categories}
+                  </select>
+                  <div className="error">{this.state.errors["category"]}</div>
+                </div>
               </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Subcategory:</label>
-                  <div className="col-4">
-                    <select className="form-control col" name="subcategory" id="category2" defaultValue={this.state.subcategory}
+                <label className="col-4 align-middle">Subcategory:</label>
+                <div className="col-4">
+                  <select className="form-control col" name="subcategory" id="category2" defaultValue={this.state.subcategory}
                     onChange={this.handleChange}>
-                      <option disabled>Please Select</option>
-                      {subcategories}
-                    </select>
-                    <div className="error">{this.state.errors["subcategory"]}</div>
-                  </div>
+                    <option disabled>Please Select</option>
+                    {subcategories}
+                  </select>
+                  <div className="error">{this.state.errors["subcategory"]}</div>
+                </div>
               </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Price:</label>
-                  <div className="col-3">
-                    <input type="text" name="price" className="form-control col" value={this.state.price}
-                    onChange={this.handleChange} required/>
-                    <div className="error">{this.state.errors["price"]}</div>
-                  </div>
-              </div>                    
+                <label className="col-4 align-middle">Price:</label>
+                <div className="col-3">
+                  <input type="text" name="price" className="form-control col" value={this.state.price}
+                    onChange={this.handleChange} required />
+                  <div className="error">{this.state.errors["price"]}</div>
+                </div>
+              </div>
               <div className="form-group row">
-                  <label className="col-4 align-middle">Stock:</label>
-                  <div className="col-3">
-                    <input type="text" name="stock" className="form-control col" value={this.state.stock} 
-                    onChange={this.handleChange} required/>
-                    <div className="error">{this.state.errors["stock"]}</div>
-                  </div>
+                <label className="col-4 align-middle">Stock:</label>
+                <div className="col-3">
+                  <input type="text" name="stock" className="form-control col" value={this.state.stock}
+                    onChange={this.handleChange} required />
+                  <div className="error">{this.state.errors["stock"]}</div>
+                </div>
               </div>
 
 
             </Modal.Body>
 
             <Modal.Footer>
-                <Button variant="primary" id="save-changes-product" type="submit">Save Changes</Button>
-                <Button variant="danger" id="delete-product" type="submit" onClick={this.deleteProduct} >Delete</Button>
-                <Button variant="secondary" onClick={this.closeModal}>Close</Button>
+              <Button variant="primary" id="save-changes-product" type="submit">Save Changes</Button>
+              <Button variant="danger" id="delete-product" type="submit" onClick={this.deleteProduct} >Delete</Button>
+              <Button variant="secondary" onClick={this.closeModal}>Close</Button>
             </Modal.Footer>
           </form>
 
