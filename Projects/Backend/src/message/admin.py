8d1b94bfe8dbfe6ac1add677e31e96e81acd6209
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2021-01-31 16:18:47 +0300

    Update admin.py

--- Projects/Backend/src/message/admin.py
+++ Projects/Backend/src/message/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from message.models import Conversation, Message, Notification
+from message.models import Conversation, Message, Notification, Report
 
 admin.site.register(Conversation)
 
 admin.site.register(Message)
 
 admin.site.register(Notification)
+
+admin.site.register(Report)
